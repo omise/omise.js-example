655eb26f5ca8da93b28a5e2eb14d266e30b560da
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2017-04-27 15:09:33 +0700

    Reduce form from two to one and remove name attribute from input element.

--- form-example/app.js
+++ form-example/app.js
@@ -4,21 +4,21 @@
 
 Omise.setPublicKey('YOUR_PUBLIC_KEY');
 
-var cardForm     = document.getElementById('card-form');
 var checkoutForm = document.getElementById('checkout-form')
 
-cardForm.addEventListener('submit', submitHandler, false);
+checkoutForm.addEventListener('submit', submitHandler, false);
 
 // Submit handler for checkout form.
 function submitHandler(event) {
   event.preventDefault();
 
+  // NOTE: Using `data-name` because we don't want to send any credit card information to the server.
   var cardInformation = {
-    name:             cardForm.nameOnCard.value,
-    number:           cardForm.cardNumber.value,
-    expiration_month: cardForm.expiryMonth.value,
-    expiration_year:  cardForm.expiryYear.value,
-    security_code:    cardForm.securityCode.value
+    name:             document.querySelector('[data-name="nameOnCard"').value,
+    number:           document.querySelector('[data-name="cardNumber"').value,
+    expiration_month: document.querySelector('[data-name="expiryMonth"').value,
+    expiration_year:  document.querySelector('[data-name="expiryYear"').value,
+    security_code:    document.querySelector('[data-name="securityCode"').value
   };
 
   Omise.createToken('card', cardInformation, function(statusCode, response) {
@@ -26,6 +26,7 @@ function submitHandler(event) {
       // Success: send back the TOKEN_ID to your server. The TOKEN_ID can be
       // found in `response.id`.
       checkoutForm.token.value = response.id;
+
       checkoutForm.submit();
     }
     else {
